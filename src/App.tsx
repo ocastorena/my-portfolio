4b79a5ab9c0545fcc690609b2052eb9d7d42e604
ocastorena <omarcastorena11@gmail.com>
2025-07-16 22:10:07 -0700

    Add and config shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white p-6">
-      <h1 className="text-5xl font-bold mb-4">Tailwind is Working!</h1>
-      <p className="text-lg text-white/80">
-        This text should be styled with Tailwind classes.
-      </p>
-      <button className="mt-6 px-4 py-2 bg-white text-blue-600 font-semibold rounded-lg shadow-md hover:bg-blue-100 transition">
-        Click Me
-      </button>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 }
